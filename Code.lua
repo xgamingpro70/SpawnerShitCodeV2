ae8f62c61582ae98307921a3e683760299f4b237
xgamingpro70 <Xgamingpro70@gmail.com>
2016-10-13 21:52:16 -0600

    Add files via upload

---
+++ Code.lua
@@ -0,0 +1,45 @@
+mon = peripheral.wrap("top")
+
+
+local function SpawnerCables()	
+
+	input = read()
+	
+	if input == "1" then
+	rs.setBundleOutput("back",colors.white)
+	elseif input == "2" then
+	rs.setBundleOutput("back",colors.orange)
+	elseif input == "3" then
+	rs.setBundleOutput("back",colors.yellow)
+	elseif input == "4" then
+	rs.setBundleOutput("back",colors.black)
+	else 
+	print("You are dumb")
+	terminal.redirect(mon1)
+end
+
+local function spawnerStatus()
+
+    local cableColor = redstone.testBundledInput(side,color)
+        if cableColor == true then
+            local status = "On"
+        else
+            local status = "Off"
+        end
+	local mobs = {"Wither Skeleton","Enderman","Blaze","Zombie Pigman","Not Bound","Not Bound","Not Bound","Not Bound"}
+    for i=1,#mobs do
+	local mon1 = term.redirect(mon)
+	term.clear()
+	term.setCursorPos(1,1)
+        print( i .. " = " .. mobs[i] )
+
+    end
+	term.redirect(mon1)
+end
+
+while true do
+SpawnerCables
+spawnerStatus
+end
+
+
