d0b538b4bdfed7ba5b0009b4392f8d61a88bdd29
xgamingpro70 <Xgamingpro70@gmail.com>
2016-10-14 15:41:28 -0600

    Update Code.lua

--- Code.lua
+++ Code.lua
@@ -1,18 +1,28 @@
 mon = peripheral.wrap("top")
- 
- 
-local function SpawnerCables() 
- 
-    input = read()
-   
-    if input == "1" then
-    rs.setBundleOutput("back",colors.white)
-    else
-    print("You are dumb")
-    terminal.redirect(mon1)
-end
- 
- 
+
+
 while true do
-SpawnerCables
+
+	input = read()
+	
+	if input == "1" then
+	rs.setBundleOutput("back",colors.white)
+	elseif input == "2" then
+	rs.setBundleOutput("back",colors.orange)
+	elseif input == "3" then
+	rs.setBundleOutput("back",colors.yellow)
+	elseif input == "4" then
+	rs.setBundleOutput("back",colors.black)
+	else 
+	print("You are dumb")
+	terminal.redirect(mon1)
+	
+
+
+
+
+
+	end
 end
+
+
