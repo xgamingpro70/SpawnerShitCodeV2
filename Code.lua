a9f137d0fc5b46ee12ab459ab6ed294c44adeb8a
xgamingpro70 <Xgamingpro70@gmail.com>
2016-10-14 15:34:32 -0600

    Update Code.lua

--- Code.lua
+++ Code.lua
@@ -1,51 +1,19 @@
 mon = peripheral.wrap("top")
-
-local mobs = {"Wither Skeleton","Enderman","Blaze","Zombie Pigman","Not Bound","Not Bound","Not Bound","Not Bound"}
-
-local function listSpawners()
-	for i=1,#mobs do
-	    print( i .. " = " .. mobs[i] )
-	end
+ 
+ 
+local function SpawnerCables() 
+ 
+    input = read()
+   
+    if input == "1" then
+    rs.setBundleOutput("back",colors.white)
+    else
+    print("You are dumb")
+    terminal.redirect(mon1)
 end
-
-function SpawnerCables()	
-	
-	print("Enter Spawner #: \n")
-	listSpawners()
-
-	input = tonumber( read() )
-	
-	if input == 1 then
-	print("Fuck You")
-	elseif input == 2 then
-	rs.setBundledOutput("back",(colors.orange) )
-	elseif input == 3 then
-	rs.setBundledOutput("back",(colors.yellow) )
-	elseif input == 4 then
-	rs.setBundledOutput("back",(colors.black) )
-	else 
-	print("You are dumb")
-	end
-end
-
-local function spawnerStatus()
-
-    local cableColor = redstone.testBundledInput(side,color)
-        if cableColor == true then
-            local status = "On"
-        else
-            local status = "Off"
-        end
-	mon.clear()
-	mon.setCursorPos(1,1)
-    for i=1,#mobs do
-	local mon1 = term.redirect(mon)
-        print( i .. " = " .. mobs[i] )
-	term.redirect(mon1)
-    end
-end
-
+ 
+ 
 while true do
-SpawnerCables()
-spawnerStatus()
+SpawnerCables
+spawnerStatus
 end
