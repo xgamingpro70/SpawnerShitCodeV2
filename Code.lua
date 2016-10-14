65a2477981c9e05780ebefd8c63940e4aaece3ba
xgamingpro70 <Xgamingpro70@gmail.com>
2016-10-14 15:44:32 -0600

    Update Code.lua

--- Code.lua
+++ Code.lua
@@ -9,12 +9,6 @@ while true do
 	
 	if input == "1" then
 	rs.setBundledOutput("back",2)
-	elseif input == "2" then
-	rs.setBundleOutput("back",colors.orange)
-	elseif input == "3" then
-	rs.setBundleOutput("back",colors.yellow)
-	elseif input == "4" then
-	rs.setBundleOutput("back",colors.black)
 	else 
 	print("You are dumb")
 	terminal.redirect(mon1)
